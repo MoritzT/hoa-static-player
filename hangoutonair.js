5600369dbb5d6ff4d79523c5cd8766943415cd16
Moritz Tolxdorff <moritz.tolxdorff@gmail.com>
2012-07-04 23:45:14 +0200

    added minified version

--- hangoutonair.js
+++ hangoutonair.js
@@ -1,1 +1,50 @@
-(function(){function a(a,c,d){var e="__YTLiveStreams_"+Math.floor(Math.random()*1000001);window[e]=d;c=c||{};c["callback"]=e;var f=b(a,c);var g=document.createElement("script");g.type="text/javascript";g.async=true;g.src=f;var h=document.getElementsByTagName("script")[0];h.parentNode.insertBefore(g,h)}function b(a,b){var c=[];for(var d in b){if(!b.hasOwnProperty(d)){continue}c.push(encodeURIComponent(d)+"="+encodeURIComponent(b[d]))}return a+"?"+c.join("&")}function c(b,c,d){var e="https://gdata.youtube.com/feeds/api/users/"+b+"/live/events";a(e,{alt:"json",v:"2"},function(a){if(a){var b=a.feed.entry[a.feed.entry.length-1].content.src.split("/");var e=b[b.length-1].split("?")[0];console.log(e);var f=document.createElement("iframe");f.src="http://www.youtube.com/embed/"+e;f.width=c||560;f.height=d||315;f.setAttribute("frameborder","0");f.setAttribute("allowfullscreen","true");var g=document.getElementById("LivePlayer");g.parentNode.insertBefore(f,g)}})}window["showLivePlayer"]=c})();
+(function(){
+    /**
+     * Private Variables and Functions
+    */                     
+    function sendRequest(url, params, callback) {
+        var magic = '__YTLiveStreams_' + Math.floor( Math.random() * 1000001 ); 
+        window[magic] = callback;
+        params = params || {};
+        params["callback"] = magic;
+        var url_ = buildURL(url, params);
+        var re = document.createElement('script'); re.type = 'text/javascript'; re.async = true;
+        re.src = url_;
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(re, s);
+    }
+    
+    function buildURL(url, params) {
+        var pairs = [];
+        for(var key in params)
+        {
+            if(!params.hasOwnProperty(key))
+            {
+            continue;
+            }
+
+            pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(params[key]));
+        }
+        return url + "?" + pairs.join("&");
+    }
+                     
+    function showLivePlayer(uid, w, h)
+    {
+        var url = "https://gdata.youtube.com/feeds/api/users/" + uid + "/live/events";
+        sendRequest(url, {alt:"json", v:"2"}, function(data){
+            if(data){
+                var parts = data.feed.entry[data.feed.entry.length-1].content.src.split("/");
+                var vid = parts[parts.length-1].split("?")[0];
+                console.log(vid);
+                var iframe = document.createElement('iframe');
+                iframe.src = 'http://www.youtube.com/embed/' + vid;
+                iframe.width = (w || 560);
+                iframe.height = (h || 315);
+                iframe.setAttribute("frameborder", "0");
+                iframe.setAttribute("allowfullscreen", "true")
+                var ref = document.getElementById("LivePlayer")
+                ref.parentNode.insertBefore(iframe, ref);
+            }
+        });
+    }
+    window["showLivePlayer"] = showLivePlayer;
+})()
